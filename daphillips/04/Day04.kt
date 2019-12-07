9c387a8a7231a1e5cc64ec04072e4a2e0ff6ce82
David Phillips <daphillips19@gmail.com>
2019-12-07 09:46:01 -0500

    days 3 and 4

---
+++ daphillips/04/Day04.kt
@@ -0,0 +1,20 @@
+fun hasDouble(digits: List<Char>): Boolean {
+    for (i in 0 until digits.size - 1) { // stops 1 short of last char so we don't compare outside of String
+        if (digits[i] == digits[i + 1] && digits.count { it == digits[i] } == 2) { // count check is part 2
+            return true
+        }
+    }
+    return false
+}
+
+fun main(args: Array<String>) {
+    val input = args.first().split('-').map { it.toInt() }
+    val bounds = input.first()..input.last()
+
+    val availablePasswords = bounds.map { it.toString().toList() }
+        .filter { it.sorted() == it }
+        .filter { hasDouble(it) }
+        .count()
+
+    println(availablePasswords)
+}
